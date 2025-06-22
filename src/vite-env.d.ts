5844b7b5339b8c7e34cde5a1ffe2ed1270129f80
Morten <enutie@gmail.com>
2025-06-22 22:59:59 +0200

    Revert "typescript?"
    This reverts commit 107611c87b7e03a44f8c082dc89cea348a6ec3f0.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,14 +1,1 @@
-// src/vite-env.d.ts
 /// <reference types="vite/client" />
-
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
-
-// You generally don't need this if "resolveJsonModule": true is in tsconfig.app.json
-// declare module '*.json' {
-//   const value: any;
-//   export default value;
-// }
